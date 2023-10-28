f2c673a2e9e1d9c155f33a5da589bf5e86fb861e
Auroes <2510407339@qq.com>
2023-10-29 00:58:18 +0800

    光线追踪外框架⭐

--- functions/QL_trees_main.m
+++ functions/QL_trees_main.m
@@ -1,34 +1,44 @@
 %QL_TREES_MAIN 林叶光线追踪主函数
-function [P,n] = QL_trees_main(za,zi,canopy_para,leaf_r,dense,D,maxN)
-%   输入参数 坡度za 方位角zi 树林模型文件名canopy_para 叶半径leaf_r 光线密度dense(0-1] 光线方向向量D 最大碰撞次数maxN
+function answer42 = QL_trees_main(za,zi,canopy_para,leaf_r,dense,D,maxN,diverN)
+%   输入参数 坡度za 方位角zi 树林模型文件名canopy_para 叶半径leaf_r 光线密度dense∈(0-1] 光线方向向量D 最大碰撞次数maxN 光线发散数diverN
 %   返回 概率P(n) n∈[1,maxN]
-global skyN % 产生的所有光线
-global hitN
-% 读入参数
-[center,edge] = leaf_position(za,zi,canopy_para,skyN); % 导出叶坐标 样地四角坐标
 
-% 光线追踪
-for i = edge/2 :-dense :-edge/2
-    for j = edge/2 :-dense :-edge/2
-        lucem = Lucem([j,i],D);
-        [skyN,hitN] = Hit(lucem,center,leaf_r,maxN); % 对每一条天空光线 返回从1到maxN次碰撞产生的所有光线skyN 其中发生碰撞的光线hitN
+    % 读入参数
+    [center,edge] = leaf_position(za,zi,canopy_para,skyN); % 导出叶坐标 样地四角坐标
+    % 中间数组 每行为对应深度 列1为碰撞总数hitN 列2为光线总数lucemN
+    hitN_lucemN = [];
+    % 结果数组 储存答案
+    answer42 = [];
+
+    % 光线追踪
+    for i = edge/2 :-dense :-edge/2
+        for j = edge/2 :-dense :-edge/2
+            % 按光线密度为步长遍历生成光线
+            lucem = Lucem([j,i],D);
+            % 对于每条(i,j)位置发射的光线 返回 每一深度 碰撞光线数量 的数组unitData
+            unitData = recDiverge(lucem,leaf_r,maxN,center,diverN);
+
+            % 遍历unitData数组的每一深度k 添加总数到中间数组
+            for k = 1:maxN
+                if k == 1
+                    if unitData{1,3}(1,2) == 0
+                        hitN_lucemN(k,1) = hitN_lucemN(k,1) + 0;%初始光线未碰撞 总碰撞光线数加0
+                        hitN_lucemN(k,2) = hitN_lucemN(k,2) + 1;%初始光线未碰撞 总光线数加1
+                    else
+                        hitN_lucemN(k,1) = hitN_lucemN(k,1) + 1;%初始光线碰撞 总碰撞光线数加1
+                        hitN_lucemN(k,2) = hitN_lucemN(k,2) + 1;%初始光线碰撞 总光线数加1
+                    end
+                else % k >=2
+                        hitN_lucemN(k,1) = hitN_lucemN(k,1) + hitN_lucemN(k-1,1) + unitData{1,3}(k,2);%从深度2开始 总碰撞光线数累加
+                        hitN_lucemN(k,2) = hitN_lucemN(k,2) + hitN_lucemN(k-1,1) + diverN*unitData{1,3}(k-1,2);%从深度2开始 总光线数等于 上一深度总光线数 与光线发散数量乘以上一深度碰撞数 的累加
+                end
+        end
     end
-end
+    
+    for m = 1:maxN
+        answer42(m,2) = hitN_lucemN(m,1)/hitN_lucemN(m/2);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-P = ;
-n = ;
+    % 返回结果数组 列1为深度n∈[1,maxN] 列2为频数P∈(0,1)
+    answer42;
 end
 
---
+++ functions/leaf_angle.m
@@ -0,0 +1,69 @@
+% Monte-Carlo ray tracing:sub-function 20230526 by Fan
+% 产生叶片的角度数据
+function [f_theta_L, nnr]=leaf_angle(f_theta,num)
+
+% f_theta_L            以1度为间隔，某种类型叶倾角分布在具体某个叶倾角上的实际概率 (%)
+% nnr                  叶片中心点法向量
+
+% f_theta              叶倾角分布代码：1 Erectophile 喜直型; 2 Planophile 喜平型; 3 Plagiophile 倾斜型; 4 Spherical 球面型; 5 Uniform 均匀型; 6 自定义叶倾角分布，读入累积概率密度分布
+% num                  叶片数量
+
+%% 计算各种叶倾角的概率
+theta_L=0:90; % 叶倾角在0~90间
+if f_theta==1
+    f_theta_L(1,:)=(2*theta_L*pi/180-sin(2*theta_L*pi/180))/pi; % 累计概率密度分布
+end
+if f_theta==2
+    f_theta_L(1,:)=(2*theta_L*pi/180+sin(2*theta_L*pi/180))/pi; % 累计概率密度分布
+end
+if f_theta==3
+    f_theta_L(1,:)=(2*theta_L*pi/180-0.5*sin(4*theta_L*pi/180))/pi; % 累计概率密度分布
+end
+if f_theta==4
+    f_theta_L(1,:)=1-cos(theta_L*pi/180); % 累计概率密度分布
+end
+if f_theta==5
+    f_theta_L(1,:)=2/pi*(theta_L*pi/180); % 累计概率密度分布
+end
+if f_theta==6 % 20211226
+    readdata=strcat('.\Model_input\leafangledistributionacc.mat'); % 读入文件
+    pp = load(readdata);
+    f_theta_L=pp.leafangledistribution;
+end
+
+% 将累计概率密度分布转换成某种类型叶倾角分布在具体某个叶倾角上的实际概率,即概率分布的离散化
+tp=[0,f_theta_L]; % 20221221
+for i=2:92 % 以1度为间隔，某种类型叶倾角分布在具体某个叶倾角上的实际概率
+    f_theta_L(1,i)=tp(:,i)-tp(:,i-1);
+end
+f_theta_L=f_theta_L(2:end); % 20221221
+
+%% 叶片中心点法向量 20211221
+% 叶片天顶角分配 20220126
+nnr_zenith_num=fix(num.*f_theta_L); % 0-90度叶倾角各自的叶片数量
+num_rest=num-sum(nnr_zenith_num); % 取整后剩余的叶片数量
+nnr_zenith_num(1:num_rest)=nnr_zenith_num(1:num_rest)+1; % 将剩余叶片数量补充到叶倾角中
+
+nnr_zenith=[];
+nnr_azimuth=[];
+for i=0:90 % 循环角度给每个叶片赋叶倾角和方位角,角度
+    nnr_zenith=[nnr_zenith; repmat(i,nnr_zenith_num(i+1),1)]; % n*1
+    
+    jg=360/nnr_zenith_num(i+1);
+    tmp=0:jg:359;
+    if nnr_zenith_num(i+1)~=0
+        nnr_azimuth=[nnr_azimuth; tmp']; % 给每个叶片赋方位角,角度
+    end
+    size_tmp=size(tmp); % 20220126
+    dif=nnr_zenith_num(i+1)-size_tmp(2); % 方位角不能恰好匹配的情况 20220126
+    if dif>0 % 20220126
+        nnr_azimuth=[nnr_azimuth; zeros(dif,1)+359.5];
+    end
+end
+
+% for i=1:num % 循环叶片数量
+%     nnr(i,:)=-sview(nnr_zenith(i,:),nnr_azimuth(i,:)); % 用计算太阳法向量的代码，根据角度计算法向量
+% end
+
+nnr=-sview(nnr_zenith,nnr_azimuth); % 用计算太阳法向量的代码，根据角度计算法向量
+
