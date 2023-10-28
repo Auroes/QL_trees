5e0224c179376787a0a9eedc09d75c26dd1dc6a7
Auroes <2510407339@qq.com>
2023-10-29 01:02:10 +0800

    🤣

---
+++ functions/recDiverge.m
@@ -0,0 +1,10 @@
+%recDiverge 发散递归函数
+function unitData = recDiverge(lucem,leaf_r,maxN,center,diverN)
+    if Hit(lucem,leaf_r,maxN,center,diverN) == 1 && diverN == 8
+        Np = recDiverge()
+
+
+
+        unitData; 
+end
+
